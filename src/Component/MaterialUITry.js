6cc95175b799186e534883bd5a1d13eda250460c
khanmd50 <md.khan@nielsen.com>
2020-03-24 20:35:36 -0700

    initial commit

---
+++ src/Component/MaterialUITry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const MaterialUITry = () => {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                 Hello World
+            </Button>
+        </div>
+    );
+};
+
+export default MaterialUITry;
